645accf1e071e87a95693c16b301cbe79c97a403
$erhat/18 <79026362+Serhat-18@users.noreply.github.com>
2021-06-21 02:29:19 +0300

    Update wl.js

--- Kayıt Bot/alchera kayıt/commands/FiveM/wl.js
+++ Kayıt Bot/alchera kayıt/commands/FiveM/wl.js
@@ -8,8 +8,6 @@ exports.run = async (client, message, args) => {
         if (!message.member.roles.cache.has(config.sorumlu)) return message.reply(config.yetersizyetkimesaj);
         const piece = message.mentions.members.first() || message.guild.members.cache.get(args[0]) 
         if (!piece) return message.channel.send(new Discord.MessageEmbed().setDescription(`Bir kullanıcı belirtmelisin. **Örnek: @Serhat**`).setFooter(config.footer).setColor("#3498DB").setTimestamp());
-        if (!Serhat) return message.channel.send(new Discord.MessageEmbed().setDescription(`İsim belirtmelisin. **Örnek: Serhat**`).setFooter(config.footer).setColor("#3498DB").setTimestamp());
-        if (!alchera) return message.channel.send(new Discord.MessageEmbed().setDescription(`Yaş belirtmelisin. **Örnek: 18**`).setFooter(config.footer).setColor("#3498DB").setTimestamp());
 
         message.channel.send(new Discord.MessageEmbed().setDescription(`**__Kayıt İşlemi Başarılı__**\n\n Kayıt Edilen Kişi: ${piece}\n Kayıt Yapan Yetkili: ${message.author}\n Kayıt İşleminde Verilen Rol: <@&${config.whrol}>\n Kayıt İşleminde Alınan Rol: <@&${config.kayitsiz}>`).setFooter(config.footer).setColor("#00dbff").setThumbnail(config.gif).setTimestamp())
         piece.setNickname(`IC ISIM`).catch(e => message.channel.send(`Benden Üstte Olduğu İçin İsmini Değiştiremedim.`))
